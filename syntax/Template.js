9fb09645a4209e692a20c3e098cb8676bccdca13
허민석 <alexhur3535@g.skky.edu>
2021-07-05 00:08:20 +0900

    Commit using git bash

---
+++ syntax/Template.js
@@ -0,0 +1,15 @@
+var name = "MINSEOK";
+var letter = 'Dear '+name+', \n\
+\
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+console.log(letter);
+
+// ` template literal을 뜻하는 문자
+
+var letter2 = `Dear ${name}, \n\
+\
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
+
+console.log(letter2);
+
+// c 언어의 %d, %c 느낌 -> template literal 필수다
